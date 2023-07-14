c319f71cd42713b32b6d481917a18270bc04357f
student <student@gnu.ac.in>
2023-07-14 08:27:53 +0530

    created practical-1.1

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,22 @@
+fun main(){
+//    println("Hello World!!")
+    var a:Int = 22
+    println("Integer value: $a")
+    var b:Double = 1.5
+    println("Double value: $b")
+    var c:Float = 1.5f
+    println("Float value: $c")
+    var d:Long = 338927847
+    println("Long value: $d")
+    var e:Short = -2
+    println("Short value: $e")
+    var f:Byte = 127
+    println("Byte value: $f")
+    var g:Char = 'T'
+    println("Char value: $g")
+    var h:Boolean = false
+    println("Boolean value: $h")
+    var i:String = "Rutul"
+    println("String value: $i")
+
+}
