ef90e11b7eade50077bc5aa07df950c3b701c63a
student <student@gnu.ac.in>
2023-07-18 08:39:13 +0530

    Completed Practical-1.3

---
+++ src/main/kotlin/Practical-1.3.kt
@@ -0,0 +1,39 @@
+fun main(){
+    println("Student Enrollment number")
+    var enroll = readLine()
+
+    println("Student Name")
+    var name = readLine()
+
+    println("Student Branch")
+    var branch = readLine()
+
+    println("Student class")
+    var c = readLine()
+
+    println("Student Batch")
+    var batch = readLine()
+
+    println("Student college name")
+    var cn = readLine()
+
+    println("Student university name")
+    var un = readLine()
+
+    println("Student age")
+    var age = readLine()
+
+    println("************************************")
+
+    println("Student's Data")
+
+    println("Enrollment Number:$enroll")
+    println("name:$name")
+    println("branch:$branch")
+    println("class:$c")
+    println("batch:$batch")
+    println("college name:$cn")
+    println("university name:$un")
+    println("age:$age")
+
+}
