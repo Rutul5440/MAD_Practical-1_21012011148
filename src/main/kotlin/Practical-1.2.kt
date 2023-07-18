483774feb1a8a97503898d89fd160b5eb88784e7
student <student@gnu.ac.in>
2023-07-18 08:11:21 +0530

    Completed Practical-1.2

--- src/main/kotlin/Practical-1.2.kt
+++ src/main/kotlin/Practical-1.2.kt
@@ -1,6 +1,15 @@
 fun main(){
     var a:Int = 10
     println("Integer value: $a")
-    a.toDouble()
+    var b = a.toDouble()
+    println("Double value from integer: $b")
+
+    var c:String = "10"
+    println("String value: $c")
+    var d = c.toInt()
+    println("Integer value from string value: $d")
+
+    var e = c.toDouble()
+    println("Double value from string value: $e")
 
 }
