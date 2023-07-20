4ae14b91b2ce504781554eb464c00dbe41e686fb
rutul <rutulsuthar2018@gmail.com>
2023-07-20 22:48:58 +0530

    completed Prctical-1.4

---
+++ src/main/kotlin/Practical-1.4kt.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Enter a number:")
+    var ui = readln().toInt()
+    if (ui / 2 == 0)
+        println("$ui number is even")
+    else
+        println("$ui number is odd")
+}
