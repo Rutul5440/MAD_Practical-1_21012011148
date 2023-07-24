92f233813577f28970721ac6b88ef809cd8089cd
rutul <rutulsuthar2018@gmail.com>
2023-07-24 23:13:51 +0530

    completed Practical-1.8

--- src/main/kotlin/Practical-1.8.kt
+++ src/main/kotlin/Practical-1.8.kt
@@ -1,5 +1,19 @@
 import java.util.*
 
+fun bubbleSort(arr: IntArray) {
+    val n = arr.size
+    for (i in 0 until n - 1) {
+        for (j in 0 until n - i - 1) {
+            if (arr[j] > arr[j + 1]) {
+                val temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+}
+
+
 fun main(){
 
     val array = arrayOf(10, 90, 60, 80, 100)
@@ -38,6 +52,46 @@ for(i in 0..4){
 //    for(i in 0..5){
 //        print("${array5[i]}, ")
 //    }
+
+
+    println("Array created using IntArrayOf()")
+    var a5 = intArrayOf(12,24,36,47,52)
+    println(a5.joinToString(", "))
+
+    println("Array created by using arrayof() and intArrayOf()")
+    var a6 = arrayOf(intArrayOf(5,3), intArrayOf(4,1), intArrayOf(6,2))
+    println(a6.contentDeepToString())
+
+    print("Enter number of Elements : ")
+    val size : Int = readLine()!!.toInt()
+    val userarray = IntArray(size) {0}
+
+
+    for(i in 0 until size)
+    {
+        print("a[$i]:")
+        userarray[i] = readLine()!!.toInt()
+    }
+    print("Entered Array = ")
+    println(userarray.contentToString())
+    val userarray1 = userarray.toMutableList()
+
+
+    println("------Sort the arry using built in function -------")
+    println("before sorting array elements are :")
+    println(userarray1.joinToString())
+    println("after sorting array elements are :")
+    userarray1.sort()
+    println(userarray1.joinToString())
+
+    println("")
+
+    println("------Sort the arry without built in function -------")
+    println("before sorting array elements are :")
+    println(userarray.joinToString())
+    println("after sorting array elements are :")
+    bubbleSort(userarray)
+    println(userarray.joinToString())
 
 
 }
