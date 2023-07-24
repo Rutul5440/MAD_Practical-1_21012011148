014fff1308981c48c1cf6ddebbba09063baa156f
rutul <rutulsuthar2018@gmail.com>
2023-07-24 23:26:03 +0530

    completed Practical-1.9

---
+++ src/main/kotlin/Practical-1.9.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val arrayList = arrayListOf(5, 10, 3, 8, 15, 7)
+
+    val maxNumber = arrayList.maxOrNull()
+
+    if (maxNumber != null) {
+        println("The maximum number is: $maxNumber")
+    } else {
+        println("The array list is empty.")
+    }
+}
